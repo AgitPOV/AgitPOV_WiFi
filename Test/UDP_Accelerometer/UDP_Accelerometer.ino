46343876fed1fb96efe5b239d875e0704c5d13a9
Thomas O Fredericks <tof@t-o-f.info>
2017-03-20 10:35:42 -0400

    Tydied up the code

--- Test/UDP_Accelerometer/UDP_Accelerometer.ino
+++ Test/UDP_Accelerometer/UDP_Accelerometer.ino
@@ -4,8 +4,8 @@
 #include <Math.h>
 
 /*
-const char* ssid = "********";
-const char* password = "********";
+  const char* ssid = "********";
+  const char* password = "********";
 */
 IPAddress myIP(192, 168, 4, 1);
 
@@ -26,7 +26,7 @@ Chrono udpSendChrono;
 #include <Adafruit_DotStar.h>
 #include <SPI.h>    // pour les dotstars
 #define NUMPIXELS 24 // Number of LEDs in strip
-#define DATAPIN    D6 
+#define DATAPIN    D6
 #define CLOCKPIN   D5
 Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BGR);
 // uint32_t color = 0x121212;  // couleur pour l'affichage d'un mot
@@ -83,101 +83,105 @@ void setup()
 void loop()
 {
 
-   if (accel.available()) {
+  if (accel.available()) {
     // First, use accel.read() to read the new variables:
     accel.read();
 
 
-   cyV = cy - cyPrevious;
-   cyVLop = (cyV - cyVLop) * 0.1 + cyVLop;
-   cyPrevious = cy;
+    cyV = cy - cyPrevious;
+    cyVLop = (cyV - cyVLop) * 0.1 + cyVLop;
+    cyPrevious = cy;
 
-   cy = accel.cy;
-   cyLop = (cy - cyLop) * 0.1 + cyLop;
+    cy = accel.cy;
+    cyLop = (cy - cyLop) * 0.1 + cyLop;
 
-   cyLopSlow = (cy - cyLopSlow) * 0.01 + cyLopSlow;
-   
-   
-   /* Moving average
-    total = total - lectures[lectureIndex];  // subtract the last reading:
-    lectures[lectureIndex] = accel.cy;
-    total = total + lectures[lectureIndex];  // add the reading to the total:
-    lectureIndex = lectureIndex + 1; // advance to the next position in the array:
-    
-    if (lectureIndex >= nmbrLectures) {   // if we're at the end of the array...
-      lectureIndex = 0;  // ...wrap around to the beginning:
-      }
+    cyLopSlow = (cy - cyLopSlow) * 0.01 + cyLopSlow;
 
-    moyenne = total / nmbrLectures;
 
-*/
+    /* Moving average
+      total = total - lectures[lectureIndex];  // subtract the last reading:
+      lectures[lectureIndex] = accel.cy;
+      total = total + lectures[lectureIndex];  // add the reading to the total:
+      lectureIndex = lectureIndex + 1; // advance to the next position in the array:
+
+      if (lectureIndex >= nmbrLectures) {   // if we're at the end of the array...
+       lectureIndex = 0;  // ...wrap around to the beginning:
+       }
+
+      moyenne = total / nmbrLectures;
+
+    */
     // calculer le laps de temps entre les deux points subséquents les plus hauts vitesseTour
-    // 
-    
-   
+    //
+
+
     //printCalculatedAccels();
     //printAccels(); // Uncomment to print digital readings
     String data = "data ";
-    data+= String(cy,3);
-    data+= " ";
-    data+= String(cyLop,3);
-    data+= " ";
-    data+= String(cyLopSlow,3);
-    data+= " ";
-    data+= String(cyV,3); // un moyenne de Y pour faire du lissage des données 
-    data+= " ";
-    data+= String(cyVLop,3); // un moyenne de Y pour faire du lissage des données 
-    
+    data += String(cy, 3);
+    data += " ";
+    data += String(cyLop, 3);
+    data += " ";
+    data += String(cyLopSlow, 3);
+    data += " ";
+    data += String(cyV, 3); // un moyenne de Y pour faire du lissage des données
+    data += " ";
+    data += String(cyVLop, 3); // un moyenne de Y pour faire du lissage des données
+
     // Calcul de l'angle...
-    // AgitAngle = round(atan2 (accel.cx,accel.cy) * -180/3.14159265 ); // ne fonctionne pas avec le X au plancher (+ de '8G') 
+    // AgitAngle = round(atan2 (accel.cx,accel.cy) * -180/3.14159265 ); // ne fonctionne pas avec le X au plancher (+ de '8G')
     // AgitAngle = round(atan2 (1,accel.cy) * -180/3.14159265 ); // X remplacé par '1' // radians to degrees and rounding
     // AgitAngle = round(atan2 (1,moyenne) * -180/3.14159265 ); // radians to degrees and rounding
     // data+= " ";
     // data+= String(AgitAngle);
-    
+
     data.toCharArray(udpMessageBuffer, 64) ;
-    
+
     Udp.beginPacket(remoteIP, remotePort);
     Udp.write(udpMessageBuffer);
     Udp.endPacket();
-    
 
-     // accel.read() will update two sets of variables. 
-    // * int's x, y, and z will store the signed 12-bit values 
+
+    // accel.read() will update two sets of variables.
+    // * int's x, y, and z will store the signed 12-bit values
     //   read out of the accelerometer.
-    // * floats cx, cy, and cz will store the calculated 
-    //   acceleration from those 12-bit values. These variables 
+    // * floats cx, cy, and cz will store the calculated
+    //   acceleration from those 12-bit values. These variables
     //   are in units of g's.
     // Check the two function declarations below for an example
     // of how to use these variables.
+    /*
+       // DO IT CODE
+      if(derniereValeur == 0 && moyenne > 0){  // on sort de la série de 0 et on augmente le compte
+         dotInit();
+        }
+          derniereValeur = moyenne;
 
-if(derniereValeur == 0 && moyenne > 0){  // on sort de la série de 0 et on augmente le compte
-     dotInit();
-    }
-      derniereValeur = moyenne;
+    */
   }
+
 
   // SEND DATA
-/*
-  if ( udpSendChrono.hasPassed(500) ) {
-    udpSendChrono.restart();
-    
-    udpMessage[0] = 1;
-    udpMessage[1] = 2;
-    udpMessage[2] = 3;
-    udpMessage[3] = 4;
-    
-     Udp.beginPacket(remoteIP, remotePort);
-    Udp.write(udpMessage);
-    Udp.endPacket();
-  }
+  /*
+    if ( udpSendChrono.hasPassed(500) ) {
+      udpSendChrono.restart();
+
+      udpMessage[0] = 1;
+      udpMessage[1] = 2;
+      udpMessage[2] = 3;
+      udpMessage[3] = 4;
+
+       Udp.beginPacket(remoteIP, remotePort);
+      Udp.write(udpMessage);
+      Udp.endPacket();
+    }
 
   */
-  
+
   /*
-  int packetSize = Udp.parsePacket();
-  if (packetSize)
-  {
+    int packetSize = Udp.parsePacket();
+    if (packetSize)
+    {
     // receive incoming UDP packets
     Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
     int len = Udp.read(incomingPacket, 255);
@@ -191,7 +195,7 @@ if(derniereValeur == 0 && moyenne > 0){  // on sort de la série de 0 et on augm
     Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
     Udp.write(replyPacekt);
     Udp.endPacket();
-  }
+    }
   */
 }
 
@@ -217,7 +221,7 @@ void printAccels()
 // Before using these variables you must call the accel.read()
 //  function!
 void printCalculatedAccels()
-{ 
+{
   Serial.print("calculated ");
   Serial.print(accel.cx, 3);
   Serial.print("\t");
@@ -229,40 +233,40 @@ void printCalculatedAccels()
 }
 
 
- void dotStop(){ // éteint toutes les lumières
-    
-   for(byte i = 0;i<=23;i++){
-      strip.setBrightness(0); 
-      strip.setPixelColor(i, 0); 
-      strip.show(); 
-      }
-  }
+void dotStop() { // éteint toutes les lumières
 
-   void dotInit(){ // séquence de départ
-    
-    dotStop();
-    
-  for(byte i = 0;i<=12;i++){ /// UP!!
-     
-      strip.setBrightness(200); 
-      strip.setPixelColor(i, color); 
-      strip.show(); 
-      yield(); // donne un peu de temps au wifi
-      delay(1);
-      yield();
-   
-      
+  for (byte i = 0; i <= 23; i++) {
+    strip.setBrightness(0);
+    strip.setPixelColor(i, 0);
+    strip.show();
+  }
+}
+
+void dotInit() { // séquence de départ
+
+  dotStop();
+
+  for (byte i = 0; i <= 12; i++) { /// UP!!
+
+    strip.setBrightness(200);
+    strip.setPixelColor(i, color);
+    strip.show();
+    yield(); // donne un peu de temps au wifi
+    delay(1);
+    yield();
+
+
     /*  for(int k = 120;k>=0;k=k-50){ /// Down
-        strip.setBrightness(k); 
-        strip.setPixelColor(i, color); 
-        strip.show(); 
+        strip.setBrightness(k);
+        strip.setPixelColor(i, color);
+        strip.show();
         yield(); // donne un peu de temps au wifi
         delay(1);
         yield();
       }*/
-     //  dotStop(); // Une seule LED allumée à la fois
+    //  dotStop(); // Une seule LED allumée à la fois
 
-    } 
-     dotStop();
-      
- } // fin dotInit
+  }
+  dotStop();
+
+} // fin dotInit
