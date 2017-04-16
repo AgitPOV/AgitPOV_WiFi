75b94377c403a42bf743d6ac2f0013fcbd953214
Alexandre Castonguay <acastonguay@artengine.ca>
2017-04-16 18:43:58 -0400

    cordova 06

--- AgitPOV/www/js/index.js
+++ AgitPOV/www/js/index.js
@@ -28,6 +28,7 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+        alert("readdy");
     },
 
     // Update DOM on a Received Event
