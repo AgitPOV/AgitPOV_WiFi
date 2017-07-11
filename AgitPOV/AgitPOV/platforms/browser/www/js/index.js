301408066bef70529fc110f4c4da04f8076db303
Alexandre Castonguay <acastonguay@artengine.ca>
2017-07-11 12:10:23 -0300

    2e hamac page

---
+++ AgitPOV/AgitPOV/platforms/browser/www/js/index.js
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        var selCouleur = 0;
+        var mot = "hello mundo";  
+        //alert(mot);
+        var form = document.getElementById("maForm");
+        // console.log("la couleur du début est : " + selCouleur);
+
+    document.getElementById("monPiton").addEventListener("click", function () {
+
+    selCouleur =  document.getElementById("maCouleur").value;
+    mot = document.getElementById("entreeTexte").value;
+
+    console.log("le mot est : " + mot);
+    console.log("la couleur est : " + selCouleur);
+
+        
+    document.getElementById("mavaleur").value = mot;
+    alert(mot);   
+    form.submit();
+    }); // fin de la fonction appelée par monPiton
+          
+        
+        
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
